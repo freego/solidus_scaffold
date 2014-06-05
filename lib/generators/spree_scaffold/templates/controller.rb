5170005fd2f4d4cdebffb331ce5af6ec247c5db7
Alessandro Lepore <lepore.ale@gmail.com>
2014-06-05 15:25:45 +0200

    remove unused controller code

--- lib/generators/spree_scaffold/templates/controller.rb
+++ lib/generators/spree_scaffold/templates/controller.rb
@@ -1,49 +1,8 @@
 module Spree
   module Admin
     class <%= class_name.pluralize %>Controller < ResourceController
-
       def index
         @<%= plural_name %> = Spree::<%= class_name %>.page(params[:page] || 1).per(50)
-      end
-
-      def new
-        @<%= singular_name %> = Spree::<%= class_name %>.new
-      end
-
-      def create
-        @<%= singular_name %> = Spree::<%= class_name %>.new(<%= singular_name %>_params)
-        if @<%= singular_name %>.save
-          flash[:success] = "Successfully created <%= singular_name %>."
-          redirect_to admin_<%= plural_name %>_url
-        else
-          render :action => 'new'
-        end
-      end
-
-      def edit
-        @<%= singular_name %> = Spree::<%= class_name %>.find(params[:id])
-      end
-
-      def update
-        @page = Spree::<%= class_name %>.find(params[:id])
-        if @<%= singular_name %>.update_attributes(<%= singular_name %>_params)
-          flash[:success] = "Successfully updated <%= singular_name %>."
-          redirect_to admin_<%= plural_name %>_url
-        else
-          render :action => 'edit'
-        end
-      end
-
-      def destroy
-        @<%= singular_name %> = Spree::<%= class_name %>.find(params[:id])
-        @<%= singular_name %>.destroy
-        flash[:success] = "Successfully destroyed <%= singular_name %>."
-        redirect_to admin_<%= plural_name %>_url
-      end
-
-      private
-      def <%= singular_name %>_params
-        params.require(:<%= singular_name %>).permit([<%= attributes.map { |attribute| ":#{attribute.name}" }.join(', ') %>])
       end
     end
   end
